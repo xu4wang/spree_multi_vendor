3fb029b34862e9bb1369325ff5c0bcf54716d9c0
Wang Xu <wangxu@mail.dspread.com>
2020-04-02 15:51:46 +0700

    add more permission for payment

--- app/models/spree/vendor_ability.rb
+++ app/models/spree/vendor_ability.rb
@@ -83,7 +83,7 @@ class Spree::VendorAbility
   end
 
   def apply_payment_permissions
-    can [:index, :create, :fire, :update], Spree::Payment
+    can [:index, :create, :fire, :new], Spree::Payment
   end
 
   def apply_shipping_methods_permissions
